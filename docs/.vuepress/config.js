044c742b5f726baca93f071b451c5cbed328e022
Bogdan Chayka <bogdan@meshnet.capital>
2024-09-23 21:16:27 -0700

    chore: remove not needed files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
-import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
+import { recoTheme } from 'vuepress-theme-reco'
 
 export default defineUserConfig({
   lang: 'en-US',
@@ -8,7 +8,7 @@ export default defineUserConfig({
   title: 'VuePress',
   description: 'My first VuePress Site',
 
-  theme: defaultTheme({
+  theme: recoTheme({
     logo: 'https://vuejs.press/images/hero.png',
 
     navbar: ['/', '/get-started'],
